f8869e043dd05eb6f3569aeaa649e979c8b12be0
Johel Roque <jroque@hunterlojack.local>
2024-11-14 22:16:11 -0500

    feat: Añade script batch para conexión SSH y gestión de procesos PM2
    Este script permite conectar de manera remota a un servidor utilizando plink.exe y ejecutar comandos en secuencia a través de SSH.
    Específicamente, navega a través de directorios, consulta el estado de los procesos de PM2, y detiene/inicia servicios como aceboot y acebook.
    Esto facilita la administración remota de servicios y asegura que los procesos en PM2 se gestionen adecuadamente desde un sistema local.

--- websuteRun.bat
+++ websuteRun.bat
@@ -1,15 +1,20 @@
-@echo off 
+@echo off
 chcp 65001 > nul
-set host= ip servidor remoto (192.14.12.1)
-set port=puerto servidor ssh
-set usuario=root
-set contraseña=servidorssh
+rem Configura la página de códigos a UTF-8 para manejar caracteres especiales.
 
+rem Definir las variables de conexión
+set host=192.14.12.1      rem Dirección IP del servidor remoto (Ejemplo: 192.14.12.1)
+set port=22               rem Puerto SSH (por defecto 22)
+set usuario=root          rem Usuario para la conexión SSH (Ejemplo: root)
+set contraseña=servidorssh rem Contraseña para la autenticación SSH (Ejemplo: "servidorssh")
+
+rem Mostrar mensaje de conexión
 echo Conectando a %usuario%@%host%:%port%...
 echo.
 echo.
 
-rem Establecer conexión SSH y ejecutar comandos en secuencia en el servidor remoto
-plink.exe -batch -P %port% %usuario%@%host% -pw %contraseña% "cd ..&& cd facebook/pm2 status && pm2 stop faceboot && pm2 start facebook"
+rem Establecer conexión SSH y ejecutar comandos en secuencia
+plink.exe -batch -P %port% %usuario%@%host% -pw %contraseña% "cd .. && cd facebook/pm2 status && pm2 stop faceboot && pm2 start facebook"
 
+rem Finalizar el script
 exit
